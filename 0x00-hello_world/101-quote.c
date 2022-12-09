117fb4f283a12fd35c71dbcc79f593b5da98a18f
rexfada <rexfada@gmail.com>
2022-12-09 09:28:42 +0100

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/**
+ * main - The entry point
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
