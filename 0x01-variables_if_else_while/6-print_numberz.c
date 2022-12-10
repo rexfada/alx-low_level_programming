04ef82658bf6288808bca2bad55af4ed5ce686bc
Emmanuel Onuh <rexfada@gmail.com>
2022-12-10 04:52:50 -0800

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - enter
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	char i;
+
+	for (i = '1'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
