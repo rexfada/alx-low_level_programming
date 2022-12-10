b298902b04215cd1a1b4e8afad89807a4dfb316b
Emmanuel Onuh <rexfada@gmail.com>
2022-12-10 05:40:24 -0800

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - enter
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= 'f'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
