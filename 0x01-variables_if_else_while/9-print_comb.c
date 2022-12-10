325f6fce59db5ccdbf903e716ae3dbd24316eb86
Emmanuel Onuh <rexfada@gmail.com>
2022-12-10 06:30:02 -0800

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - enter
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 56; i++)
+	{
+	putchar( i);
+	putchar (',');
+	putchar (' ');
+	}
+	putchar (57);
+	putchar('\n');
+	return (0);
+}
+
