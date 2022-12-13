0980d0929a6ed15079093ef7f8feb1df396ce8a9
Emmanuel Onuh <rexfada@gmail.com>
2022-12-13 17:02:01 +0100

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Tis program prints _putchar
+ *
+ * Return: Returns 0
+ *
+ */
+int main(void)
+{
+	char _putchar;
+
+	putchar(_putchar);
+	putchar('\n');
+
+
+	return (0);
+}
