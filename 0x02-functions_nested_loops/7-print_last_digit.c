31f53a63e0f079a0b5d2b07934ffbacd9f4c8efd
Emmanuel Onuh <rexfada@gmail.com>
2022-12-14 13:45:12 +0100

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,7 +8,18 @@
  */
 int print_last_digit(int x)
 {
-	int y = ((x % 10));
+	int y;
+
+	if (x < 0)
+	{
+		x = -x;
+	}
+	y = x % 10;
+	if (y < 0)
+	{
+		y = -y;
+	}
+	_putchar(y + '0');
 
 	return (y);
 
