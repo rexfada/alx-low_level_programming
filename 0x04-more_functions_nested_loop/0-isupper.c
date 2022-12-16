dbc0267f0cd71eedf596286bba4efc6781628399
Emmanuel Onuh <rexfada@gmail.com>
2022-12-16 09:00:07 +0100

    upper case

---
+++ 0x04-more_functions_nested_loop/0-isupper.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* _isupper - function to check forupper case character
+* @c: the test character
+* Return: the reurned character
+*/
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
