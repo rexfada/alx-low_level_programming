746deeeb4d7aac4c543a4313aedfba890732be69
Emmanuel Onuh <rexfada@gmail.com>
2022-12-16 20:37:13 +0100

    updated

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* more_numbers - program that print the numbers 0 to 14 ten times
+*
+*/
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
