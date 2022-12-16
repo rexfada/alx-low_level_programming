749498128582a74ce29841671fba897ef8eb7cd2
Emmanuel Onuh <rexfada@gmail.com>
2022-12-16 20:21:49 +0100

    updated

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_numbers - program that prints numbers 1 to 9
+*Return: 0
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
