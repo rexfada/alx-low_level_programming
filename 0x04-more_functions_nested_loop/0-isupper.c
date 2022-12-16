df738dbefd99b49715d3e2412ed2eaaa0c4367cd
Emmanuel Onuh <rexfada@gmail.com>
2022-12-16 00:15:00 -0800

    is upper

---
+++ 0x04-more_functions_nested_loop/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isupper - program to check for uppercase
+ * @c: the test character
+ * Return: alphabet
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
