4ee628b1e8861d27541f85f5ebcde1afb085bc95
Emmanuel Onuh <rexfada@gmail.com>
2022-12-16 21:23:35 +0100

    updated

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,7 +10,15 @@ void print_line(int n)
 
 	for (i = 0; i <= n; i++)
 	{
-		_putchar('_');
+		if (n <= 0)
+		{
+			break;
+		}
+		else
+		{
+			continue;
+			_putchar('_');
+		}
 	}
 	_putchar ('\n');
 }
