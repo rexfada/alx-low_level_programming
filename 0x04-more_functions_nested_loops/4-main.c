5522ba3d5227c4c3a5a86888a53631769cb528e0
Emmanuel Onuh <rexfada@gmail.com>
2022-12-17 03:50:21 +0100

    updated

---
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * print_most_numbers - program to print most nubers without 2 and 4
+ * Return: Always 0.
+ */
+void print_most_numbers(void);
+int main(void)
+{
+	print_most_numbers();
+	return (0);
+}
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,13 +5,13 @@
 */
 void print_most_numbers(void)
 {
-	int i;
+	char i;
 
-	for (i = 0; i <= 10; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		if (i != 2 && i != 4)
+		if (i != '2' && i != '4')
 		{
-			_putchar(i + '0');
+			_putchar(i);
 		}
 	}
 	_putchar('\n');
