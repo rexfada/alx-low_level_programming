7520fd23a48933c5960cdb5c47eb6429be3150ee
Emmanuel Onuh <rexfada@gmail.com>
2022-12-20 20:28:57 +0100

    puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ *puts_half - function a string
+ *@str: varible punter
+ */
+void puts_half(char *str)
+{
+	int c = 0;
+	int n;
+	int j;
+
+	while (str[c] != '\0')
+
+		c++;
+
+	if (c % 2 == 0)
+
+	n = c / 2;
+
+	else
+	n = ((c - 1) / 2) + 1;
+
+
+	for (j = n; j < c; j++)
+	{
+
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}
