06bb1ebdaca8e685aaf9fdfa2dfed7bfaae450f5
Emmanuel Onuh <rexfada@gmail.com>
2022-12-20 20:47:33 +0100

    print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include"main.h"
+/**
+* print_array - funtion that prints n elements of an integer array
+* @a: the array
+* @n: the no of elemens in the array
+*/
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; ++i)
+	{
+		if (i != (n - 1))
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d,", a[i]);
+		}
+	}
+	printf("\n");
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include<stdio.h>
 
-
-
+void print_array(int *a, int n);
 void puts_half(char *str);
 void puts2(char *str);
 void rev_string(char *s);
