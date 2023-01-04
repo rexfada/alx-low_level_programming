8b3eea288a2f18f333270a653bb111e3e2bb0e03
Emmanuel Onuh <rexfada@gmail.com>
2023-01-04 11:11:19 +0100

    string reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+* _print_rev_recursion - function that print a strig in reverse using recursion
+* @s: the string
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
