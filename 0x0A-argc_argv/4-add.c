65f448774fb53803123fa4b982f0b4d96697472d
Emmanuel Onuh <rexfada@gmail.com>
2023-01-10 07:28:54 +0100

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+/**
+* main - program that adds numbers
+* @argc: the count of arguments
+* @argv: the arguments
+* Return: 0 on success
+*/
+int main(int argc, char *argv[])
+{
+	int sum = 0, i, j;
+
+	if (argc == 0)
+	{
+		printf("0\n");
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+				{
+					printf("Error\n");
+					return (1);
+				}
+		}
+	sum += atoi(argv[i]);
+	}		
+	printf("%i\n", sum);
+	return (0);
+}
