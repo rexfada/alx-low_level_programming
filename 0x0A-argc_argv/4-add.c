82f0945a14bcd040c21b89e830135c691762da8b
Emmanuel Onuh <rexfada@gmail.com>
2023-01-10 07:31:47 +0100

    updated

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -20,13 +20,13 @@ int main(int argc, char *argv[])
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (!isdigit(argv[i][j]))
-				{
-					printf("Error\n");
-					return (1);
-				}
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
 	sum += atoi(argv[i]);
-	}		
+	}
 	printf("%i\n", sum);
 	return (0);
 }
