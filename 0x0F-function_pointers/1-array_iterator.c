fea0fa56d85276f8e58a624375a2dd0623156f3e
rexfada <rexfada@gmail.com>
2023-01-19 17:38:42 +0100

    update

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,9 +10,11 @@
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i;
-
-	if (array != NULL && size > 0 && action != NULL)
-		for (i = 0; i < size; i++)
-			action(array[i]);
+	if (array == NULL || action == NULL)
+		return;
+	while (size-- > 0)
+	{
+		action(*array);
+		array++;
+	}
 }
