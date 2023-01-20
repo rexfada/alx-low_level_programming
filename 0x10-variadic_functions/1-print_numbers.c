59cb8a3408ae7f345374ec801e1bc4334c86da0b
rexfada <rexfada@gmail.com>
2023-01-20 20:50:14 +0100

    updated

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,9 +16,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%i", va_arg(numbers, int));
 		if (separator && i != n - 1)
-		{
 			printf("%s", separator);
-		}
 	}
 	va_end(numbers);
 	printf("\n");
