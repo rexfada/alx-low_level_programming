7bbbcd98966ddcb9210907a652116afb43af27ff
rexfada <rexfada@gmail.com>
2023-01-21 13:02:17 +0100

    updated

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,8 +15,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		str = va_arg(strings, char *);
 		if (str == NULL)
+		{
 			printf("(null)");
-		printf("%s", str);
+		}
+		else
+		{
+			printf("%s", str);
+		}
 		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
 	}
