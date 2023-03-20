c0b62e49eb78c16938c3b245fccc5b259722ec78
rexfada <rexfada@gmail.com>
2023-03-20 19:36:48 +0100

    task 2 updated

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* add - function to add 2 numbers
+* @a: the first operand
+* @b: the second operand
+* Return: the resultant operation
+*/
+
+int add(int a, int b)
+{
+    return(a + b);
+}
+
+ /**
+ * sub - function to subtract 2 numbers
+ * @a: the first operand
+ * @b: the second operand
+ * Return: the resultant operation
+ */
+ int sub(int a, int b)
+ {
+     return(a - b);
+ }
+
+ /**
+ * mul - function to multiply 2 numbers
+ * @a: the first operand
+ * @b: the second operand
+ * Return: the resultant operation
+ */
+ int mul(int a, int b)
+ {
+     return(a * b);
+ }
+
+
+ /**
+ * div - function to divide 2 numbers
+ * @a: the first operand
+ * @b: the second operand
+ * Return: the resultant operation
+ */
+ int div(int a, int b)
+ {
+     return(a / b);
+ }
+
+
+ /**
+ * mod - function to get modulus
+ * @a: the first operand
+ * @b: the second operand
+ * Return: the resultant operation
+ */
+ int mod(int a, int b)
+ {
+     return(a % b);
+ }
